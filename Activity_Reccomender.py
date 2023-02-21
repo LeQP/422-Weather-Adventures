a0466bd82d3b5a84c494db692ad5a180b7c7d0cb
Angela Pelky <angelapelky@Angelas-MacBook-Pro-2.local>
2023-02-21 11:00:54 -0800

    Activity reccomender notes and pseudocode

---
+++ Activity_Reccomender.py
@@ -0,0 +1,10 @@
+'''
+Psuedocode
+- Data throughout Oregon (maybe we start with just Eugene first) that includes outdoor activities
+- Data connected to the grid so that there's at least one reccomendation per grid square
+- When the grid is clicked we check the weather
+- When we check the weather we spit yes or no
+Ideas for data
+- All trails
+- Oregon gov
+'''
