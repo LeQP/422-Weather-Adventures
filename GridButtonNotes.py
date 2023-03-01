552b72db49324f6f3c5dd665dd85f9b82b56e51c
Joey Le <qle@uoregon.edu>
2023-02-28 17:04:39 -0800

    Added notes for changing button images when using other buttons

--- GridButtonNotes.py
+++ GridButtonNotes.py
@@ -1,12 +1,55 @@
 # This file describes an idea I have for making an interactable map by using images as buttons
 # Could not exactly put a new grid on a prexisting image (as of now), so I'm making the images become the grid
 
-# Function for getting buttons
+# Function for getting buttons; provide label for number clicked
 def buttonFunc(coordinates:str):
     inputStr = "Button Clicked at " + coordinates
     labelButton.configure(text=inputStr)
 
-
+# Function for the Zoom Buttons; update the number images, 
+#   zoom in = decrease val, zoom out = increase val
+#   Use .configure(image =) to update image
+#   Update num after images are reconfigured
+def zoom(closer:bool):
+    # Acces global variable with global
+    global num 
+    # Zoom Out
+    if (closer == False):
+        if (num == 3):
+            labelZ.configure(text = "Can't zoom out any farther")
+        elif (num == 1):
+            labelZ.configure(text = "Number zoomed out to 2")
+            buttonA.configure(image = windowImage2)
+            buttonB.configure(image = windowImage2)
+            buttonC.configure(image = windowImage2)
+            buttonD.configure(image = windowImage2)
+            num = 2
+        elif (num == 2):
+            labelZ.configure(text = "Number zoomed out to 3")
+            buttonA.configure(image = windowImage3)
+            buttonB.configure(image = windowImage3)
+            buttonC.configure(image = windowImage3)
+            buttonD.configure(image = windowImage3)
+            num = 3
+    # Zoom in
+    else:
+        if (num == 1):
+            labelZ.configure(text = "Can't zoom in any closer")
+        elif (num == 3):
+            labelZ.configure(text = "Number zoomed in to 2")
+            buttonA.configure(image = windowImage2)
+            buttonB.configure(image = windowImage2)
+            buttonC.configure(image = windowImage2)
+            buttonD.configure(image = windowImage2)
+            num = 2
+        elif (num == 2):
+            labelZ.configure(text = "Number zoomed in to 1")
+            buttonA.configure(image = windowImage1)
+            buttonB.configure(image = windowImage1)
+            buttonC.configure(image = windowImage1)
+            buttonD.configure(image = windowImage1)
+            num = 1
+        
 ''' Imports '''
 # import tkinter module
 from tkinter import * 
@@ -25,21 +68,48 @@ window.geometry("750x750")
 
 ''' Add image (needs to be PNG) '''
 cwd = os.getcwd()
-path = cwd + "/testImages/one.png"
-openImage = Image.open(path)
-resizeImage = openImage.resize((150, 150))
-windowImage = ImageTk.PhotoImage(resizeImage)
-button1a = Button(window, image = windowImage, command=lambda: buttonFunc("(0,0)"))
-button1a.grid(row = 0, column = 0)
-button1b = Button(window, image = windowImage, command=lambda: buttonFunc("(0,1)"))
-button1b.grid(row = 0, column = 1)
-button1c = Button(window, image = windowImage, command=lambda: buttonFunc("(1,0)"))
-button1c.grid(row = 1, column = 0)
-button1d = Button(window, image = windowImage, command=lambda: buttonFunc("(1,1)"))
-button1d.grid(row = 1, column = 1)
-labelButton = Label(window, text = "")
-labelButton.grid(row = 2, column = 2)
+# 1. Set the path
+path1 = cwd + "/testImages/one.png"
+path2 = cwd + "/testImages/two.png"
+path3 = cwd + "/testImages/three.png"
 
+# 2. Open the images
+openImage1 = Image.open(path1)
+openImage2 = Image.open(path2)
+openImage3 = Image.open(path3)
+
+# 3. Resize them
+resizeImage1 = openImage1.resize((150, 150))
+resizeImage2 = openImage2.resize((150, 150))
+resizeImage3 = openImage3.resize((150, 150))\
+
+# 4. Set them to be on Tkinter
+windowImage1 = ImageTk.PhotoImage(resizeImage1)
+windowImage2 = ImageTk.PhotoImage(resizeImage2)
+windowImage3 = ImageTk.PhotoImage(resizeImage3)
+
+# Place the image buttons
+buttonA = Button(window, image = windowImage1, command=lambda: buttonFunc("(0,0)"))
+buttonA.grid(row = 0, column = 0)
+buttonB = Button(window, image = windowImage1, command=lambda: buttonFunc("(0,1)"))
+buttonB.grid(row = 0, column = 1)
+buttonC = Button(window, image = windowImage1, command=lambda: buttonFunc("(1,0)"))
+buttonC.grid(row = 1, column = 0)
+buttonD = Button(window, image = windowImage1, command=lambda: buttonFunc("(1,1)"))
+buttonD.grid(row = 1, column = 1)
+labelButton = Label(window, text = "")
+labelButton.grid(row = 1, column = 2)
+
+# num is used to keep track of which number is on display
+num = 1
+
+# Add buttons to change the image
+buttonIn = Button(window, text = "ZOOM IN", command=lambda: zoom(True))
+buttonOut = Button(window, text = "ZOOM OUT", command=lambda: zoom(False))
+buttonIn.grid(row = 3, column = 0)
+buttonOut.grid(row = 3, column = 1)
+labelZ = Label(window, text = "")
+labelZ.grid(row = 3, column = 2)
 
 
 mainloop()
