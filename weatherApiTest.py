590df92cb4d283f41d11383e0a4cbe7681eed8f6
merriset <109322248+merriset@users.noreply.github.com>
2023-03-01 15:47:27 -0800

    added symbol path generation

--- weatherApiTest.py
+++ weatherApiTest.py
@@ -4,7 +4,7 @@ import requests
 import json
 import sys
 
-def getWeatherInfo(lat, lon, units, request):
+def getWeatherInfo(lat, lon, units):
 
     query = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + units + "&appid=f69661f19f90747329bfc3fe6cbc1d0d"
     response = requests.get(query)
@@ -13,29 +13,58 @@ def getWeatherInfo(lat, lon, units, request):
 
     data = json.loads(json_object)
 
-    if request == 'desc':
-        print(data['weather'][0]['description'])
+    print("WEATHER DESCRIPTION: " + data['weather'][0]['description'])
+    weatherSymStr = "/images/weatherSymbols/" + weatherSymLookup(data['weather'][0]['description'])
+    print("WEATHER SYMBOL PATH: " + weatherSymStr)
+ 
+    print("TEMP: "+ str(data['main']['temp']))
 
-    elif request == 'temp': 
-        print(data['main']['temp'])
-
-    elif request == 'wind':
-        print(data['wind']['speed'])
-        print(data['wind']['deg'])
-        print(data['wind']['gust'])
-
+    print("AVG WIND SPEED: " + str(int(data['wind']['speed']) + int(data['wind']['gust']) / 2))
+    print("WIND DIRECTION: " + str(data['wind']['deg']))
+    
+    windDir = int(data['wind']['deg'])
+    if (windDir % 15) < 7:
+        windDirRounded = windDir - (15 - windDir % 15)
     else:
-        sys.exit("Invalid request")
+        windDirRounded = windDir + (15 - windDir % 15)
+
+    # not sure about this logic
+    if windDirRounded < 0:
+        windDirRounded = 0
+    elif windDirRounded > 345:
+        windDirRounded = 345
+
+    windSymFPath = "/images/weatherSymbols/w" + str(windDirRounded)
+    print("WIND SYMBOL PATH: " + windSymFPath)            
+
+def weatherSymLookup(weatherStr):
+
+    dict = {
+        "few clouds": "broken_clouds.png",
+        "scattered clouds": "broken_clouds.png",
+        "broken clouds": "overcast.png",
+        "overcast clouds": "overcast.png",
+
+        "clear sky": "clear.png",
+
+        "light snow": "snow.png",
+        "snow": "snow.png",
+        "heavy snow": "snow.png",
+
+        "light rain": "rain.png",
+        "heavy intensity rain": "rain.png",
+        "moderate rain": "rain.png",
+        "very heavy rain": "rain.png",
+
+        # add more cases
+        "drizzle" : "rain.png"
+    }
+
+    return dict[weatherStr]
 
 
 lat = input("Enter lat: \n")
 lon = input("Enter lon: \n")
 units = input("Enter units: (imperial, standard, metric) \n")
-request = input("Enter request (desc, wind, temp): \n")
-getWeatherInfo(lat, lon, units, request)
-
-
-# DEBUG 
-# with open("sample.json", "w") as outfile:
-#     outfile.write(json_object)
+getWeatherInfo(lat, lon, units)
 
