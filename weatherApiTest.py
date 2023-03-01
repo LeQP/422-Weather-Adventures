05f92a365adf98ba76b86c959849e5c60816ae13
merriset <109322248+merriset@users.noreply.github.com>
2023-03-01 14:55:14 -0800

    added units option

--- weatherApiTest.py
+++ weatherApiTest.py
@@ -4,31 +4,35 @@ import requests
 import json
 import sys
 
+def getWeatherInfo(lat, lon, units, request):
+
+    query = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + units + "&appid=f69661f19f90747329bfc3fe6cbc1d0d"
+    response = requests.get(query)
+
+    json_object = json.dumps(response.json(), indent=4)
+
+    data = json.loads(json_object)
+
+    if request == 'desc':
+        print(data['weather'][0]['description'])
+
+    elif request == 'temp': 
+        print(data['main']['temp'])
+
+    elif request == 'wind':
+        print(data['wind']['speed'])
+        print(data['wind']['deg'])
+        print(data['wind']['gust'])
+
+    else:
+        sys.exit("Invalid request")
+
+
 lat = input("Enter lat: \n")
 lon = input("Enter lon: \n")
+units = input("Enter units: (imperial, standard, metric) \n")
 request = input("Enter request (desc, wind, temp): \n")
-
-query = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=f69661f19f90747329bfc3fe6cbc1d0d"
-response = requests.get(query)
-
-json_object = json.dumps(response.json(), indent=4)
-
-data = json.loads(json_object)
-
-if request == 'desc':
-    print(data['weather'][0]['description'])
-
-elif request == 'temp': 
-    print(data['main']['temp'])
-
-elif request == 'wind':
-    print(data['wind']['speed'])
-    print(data['wind']['deg'])
-    print(data['wind']['gust'])
-
-else:
-    sys.exit("Invalid request")
-
+getWeatherInfo(lat, lon, units, request)
 
 
 # DEBUG 
